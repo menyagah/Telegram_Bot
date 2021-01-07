b52274f4aa5c3677e390215ef36ce5ed14acb605
menyagah <menyagah27@gmail.com>
2021-01-07 18:38:28 +0300

    Separate bot class inherit test to its own file

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,19 +1,7 @@
 require_relative '../lib/bot'
-require_relative '../lib/user_inputs'
-
-describe Input do
-  describe '#user_choice' do
-    it 'should return the choice item if the message is equal to the choice item' do
-      expect(subject.user_choice('yes 3', ['yes 1', 'yes 2', 'yes 3', 'yes 3', 'yes 4'])).to eql('yes 3')
-    end
-  end
-  describe '#user_option' do
-    it 'should return the matching item in the hash case' do
-      expect(subject.user_option('/iphone 5')).to eql('/yes 5')
-    end
-  end
-end
 
 describe Bot do
-  it { expect(described_class).to be < Input }
+    it "Should inherit Input class" do
+         expect(described_class).to be < Input 
+    end
 end
