c54db08c7e7ca291e749488749a4996f9f6d3e40
menyagah <menyagah27@gmail.com>
2021-01-07 12:07:33 +0300

    Add Input passing test

---
+++ spec/bot_spec.rb
@@ -0,0 +1,10 @@
+require '../lib/bot'
+require '../lib/user_inputs'
+
+describe Input do
+    describe "#user_choice" do
+        it "should return the choice item if the message is equal to the choice item" do
+            expect(subject.user_choice('yes 3',['yes 1','yes 2','yes 3','yes 3','yes 4'])).to eql('yes 3')
+        end
+    end
+end
