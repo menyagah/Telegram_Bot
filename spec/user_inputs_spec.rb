30d5bc8d38471f714ec5412f850a0a4de2e88e51
menyagah <menyagah27@gmail.com>
2021-01-07 19:09:08 +0300

    Add negative test case scenarios

--- spec/user_inputs_spec.rb
+++ spec/user_inputs_spec.rb
@@ -6,10 +6,16 @@ describe Input do
     it 'should return the choice item if the message is equal to the choice item' do
       expect(subject.user_choice('yes 3', ['yes 1', 'yes 2', 'yes 3', 'yes 3', 'yes 4'])).to eql('yes 3')
     end
+    it 'should return nil if the message is not equal to the choice item' do
+      expect(subject.user_choice('yes', ['yes 1', 'yes 2', 'yes 3', 'yes 3', 'yes 4'])).to eql(nil)
+    end
   end
   describe '#user_option' do
     it 'should return the matching item in the hash case' do
       expect(subject.user_option('/iphone 5')).to eql('/yes 5')
+    end
+    it 'should return nil if the matching item is not in the hash case' do
+      expect(subject.user_option(5)).to eql(nil)
     end
   end
 end
