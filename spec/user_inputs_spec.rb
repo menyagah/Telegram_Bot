53cd17d4c4d55714c8e775cd269dad2f9a147703
menyagah <menyagah27@gmail.com>
2021-01-13 19:44:12 +0300

    Remove test from user_inputs_spec folder and create new class

--- spec/user_inputs_spec.rb
+++ spec/user_inputs_spec.rb
@@ -10,12 +10,4 @@ describe Input do
       expect(subject.user_choice('yes', ['yes 1', 'yes 2', 'yes 3', 'yes 3', 'yes 4'])).to eql(nil)
     end
   end
-  describe '#user_option' do
-    it 'should return the matching item in the hash case' do
-      expect(subject.user_option('/iphone 5')).to eql('/yes 5')
-    end
-    it 'should return nil if the matching item is not in the hash case' do
-      expect(subject.user_option(5)).to eql(nil)
-    end
-  end
 end
