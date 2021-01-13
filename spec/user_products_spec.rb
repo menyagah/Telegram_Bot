2c51b2441dbfdde511e075abc608ee93f522d00a
menyagah <menyagah27@gmail.com>
2021-01-13 19:46:00 +0300

    Add testcase for user_products

--- spec/user_products_spec.rb
+++ spec/user_products_spec.rb
@@ -1,2 +1,12 @@
 require_relative '../lib/user_products'
 
+describe Product do 
+    describe '#user_option' do
+    it 'should return the matching item in the hash case' do
+      expect(subject.user_option('/iphone 5')).to eql('/yes 5')
+    end
+    it 'should return nil if the matching item is not in the hash case' do
+      expect(subject.user_option(5)).to eql(nil)
+    end
+  end
+end
